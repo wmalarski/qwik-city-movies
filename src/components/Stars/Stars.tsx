2d567018af4932a06c7edf21c4b0e3b74821c55c
Wojciech Malarski <w.malarski@gmail.com>
2023-05-02 10:37:49 +0200

    packages bumped

--- src/components/Stars/Stars.tsx
+++ src/components/Stars/Stars.tsx
@@ -1,11 +1,13 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useComputed$ } from "@builder.io/qwik";
 
 type Props = {
   rating?: number;
 };
 
 export const Stars = component$((props: Props) => {
-  const value = Math.round((props.rating || 0) * 10) / 10;
+  const stars = useComputed$(() => {
+    return Math.round((props.rating || 0) * 10) / 10;
+  });
 
   return (
     <div class="relative flex flex-row items-center gap-2">
@@ -14,9 +16,9 @@ export const Stars = component$((props: Props) => {
         alt="rating"
         class="absolute h-3 w-20"
         src="/images/stars-filled.png"
-        style={{ clipPath: `inset(0px ${100 - value * 10}% 0px 0px)` }}
+        style={{ clipPath: `inset(0px ${100 - stars.value * 10}% 0px 0px)` }}
       />
-      <div class="text-sm opacity-80">{value}</div>
+      <div class="text-sm opacity-80">{stars.value}</div>
     </div>
   );
 });
