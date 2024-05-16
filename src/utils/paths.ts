c7f122ff9f1aba07f33c8d00ab36201123bd74b9
Wojciech Malarski <w.malarski@gmail.com>
2024-05-17 00:31:13 +0200

    changes paths

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,17 +2,17 @@ import { MediaBase } from "~/services/types";
 
 export const paths = {
   genre: (mediaType: MediaBase["media_type"], id: number) =>
-    `/genre/${id}/${mediaType}`,
+    `/genre/${id}/${mediaType}/`,
   index: "/",
   media: (mediaType: MediaBase["media_type"], id: number) =>
-    `/${mediaType}/${id}`,
-  movieCategory: (category: string) => `/movie/categories/${category}`,
-  moviePhotos: (id: number) => `/movie/${id}/photos`,
-  movieVideo: (id: number) => `/movie/${id}/videos`,
-  movies: "/movie",
-  notFound: "/404",
-  person: (id: number) => `/person/${id}`,
-  search: "/search",
-  tv: "/tv",
-  tvCategory: (category: string) => `/tv/categories/${category}`,
+    `/${mediaType}/${id}/`,
+  movieCategory: (category: string) => `/movie/categories/${category}/`,
+  moviePhotos: (id: number) => `/movie/${id}/photos/`,
+  movieVideo: (id: number) => `/movie/${id}/videos/`,
+  movies: "/movie/",
+  notFound: "/404/",
+  person: (id: number) => `/person/${id}/`,
+  search: "/search/",
+  tv: "/tv/",
+  tvCategory: (category: string) => `/tv/categories/${category}/`,
 };
